05d2618b93257343df64d1185a0e834ffe06487f
minemobs <issahakim94@gmail.com>
2023-08-20 20:35:24 +0200

    Added day-02 and day-03

---
+++ day-02/index.ts
@@ -0,0 +1,65 @@
+const choice = {
+  rock: ['A', 'X'],
+  paper: ['B', 'Y'],
+  sissors: ['C', 'Z']
+} as const;
+
+type Choice = typeof choice[keyof typeof choice][number];
+type PlayerChoice = Exclude<Choice, "X" | "Y" | "Z">;
+type EncryptedChoice = Exclude<Choice, "A" | "B" | "C">;
+
+function toChoice(encriptedChoice: EncryptedChoice) : PlayerChoice {
+  switch(encriptedChoice) {
+    case 'X': return 'A';
+    case 'Y': return 'B';
+    case 'Z': return 'C';
+  }
+}
+
+function choiceToPoint(playerChoice: PlayerChoice) {
+  if(playerChoice === choice.rock[0]) return 1;
+  else if(playerChoice === choice.paper[0]) return 2;
+  else return 3;
+}
+
+function rpsVerifyUsingChoices(playerChoice: PlayerChoice, encriptedChoice: EncryptedChoice) : number {
+  const yourChoice = toChoice(encriptedChoice);
+  let points = choiceToPoint(yourChoice);
+  //Verfication
+  if(playerChoice === yourChoice) return points + 3;
+  const winPairs : Record<PlayerChoice, PlayerChoice> = {
+    'A': 'B',
+    'B': 'C',
+    'C': 'A',
+  } as const;
+  if(winPairs[playerChoice] === yourChoice) return points + 6;
+  return points;
+}
+
+const choices = ["A", "B", "C"] as const;
+const at = (i: number) : PlayerChoice => choices[(i % choices.length + choices.length) % choices.length]!;
+function twoStarsRPSVerify(playerChoice: PlayerChoice, encriptedChoice: EncryptedChoice): number {
+  if(encriptedChoice === 'Y') return choiceToPoint(playerChoice) + 3;
+  else if(encriptedChoice === 'Z') return choiceToPoint(at(choices.indexOf(playerChoice) + 1)) + 6;
+  else return choiceToPoint(at(choices.indexOf(playerChoice) - 1));
+}
+
+function rpsVerify(line: string, twoStars : boolean = false) : number {
+  const choices = line.split(" ") as [PlayerChoice, EncryptedChoice];
+  return twoStars ? twoStarsRPSVerify(choices[0], choices[1]) : rpsVerifyUsingChoices(choices[0], choices[1]);
+}
+
+const test = false;
+
+const file = Bun.file(`day-02/input-${test ? 'test' : 'full'}.txt`);
+const text = await file.text();
+const lines = text.split("\n");
+let score = 0;
+
+for(let i = 0; i < lines.length; i++) {
+  const line = lines[i];
+  if(line === undefined || line.length === 0) continue;
+  score += rpsVerify(line, true);
+}
+  
+console.log(`Your score is: ${score}`);
---
+++ day-03/index.ts
@@ -0,0 +1,40 @@
+import { readFileSync } from 'fs';
+
+const test = false;
+const content = readFileSync(`day-03/input-${test ? "test" : "full"}.txt`, { encoding: "utf8" })
+  .replace("/\r/g", "")
+  .trim()
+  .split("\n");
+
+function firstPart() {
+  let sum = 0;
+  for(const line of content) {
+    const firstCompartment = line.substring(0, line.length / 2);
+    const secondCompartment = line.substring(line.length / 2);
+    const char = [...secondCompartment].find(c => firstCompartment.indexOf(c) !== -1);
+    if(char === undefined) throw new Error("Couldn't find repeated letter");
+    sum += toPriority(char);
+  }
+  console.log(sum);
+}
+
+function secondPart() {
+  let sum = 0;
+  for(let i = 0; i < content.length; i += 3) {
+    const firstCompartment = content[i]!;
+    const secondCompartment = content[i + 1]!;
+    const thirdCompartment = content[i + 2]!;
+    const char = [...secondCompartment].find(c => firstCompartment.indexOf(c) !== -1 && thirdCompartment.indexOf(c) !== -1);
+    if(char === undefined) throw new Error("Couldn't find repeated letter");
+    sum += toPriority(char);
+  }
+  console.log(sum);
+}
+
+function toPriority(char: string) {
+  if(char.charCodeAt(0) <= 90) return char.charCodeAt(0) - 65 + 27;
+  return char.charCodeAt(0) - 97 + 1;
+}
+
+//firstPart();
+secondPart();
