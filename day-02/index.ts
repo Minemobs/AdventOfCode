0b2d04716f4b54775d726443935fb32781199cbd
minemobs <issahakim94@gmail.com>
2023-12-02 19:50:30 +0100

    Added day-02 (2nd part)

--- day-02/index.ts
+++ day-02/index.ts
@@ -3,22 +3,24 @@ import { getContent, isNotBlank } from "../utils";
 const test = false;
 const content = await getContent(test).then(it => it.split("\n").filter(isNotBlank));
 
-// First part
-
 type ColoredBox = { red: number, green: number, blue: number };
+
+function parse(line: string) {
+    return line.split(": ")[1]!.split("; ").map(it => it.split(", ").map(l => l.split(" ") as [string, keyof ColoredBox]));
+}
+
+// First part
+console.log("First Part");
 
 function firstPart() {
   let sum = 0;
   for(let id = 0; id < content.length; id++) {
-    const items = parseFirstPart(content[id]!);
+    const items = parse(content[id]!);
     if(items.find(isOverflowing) === undefined) sum += id + 1;
   }
   console.log(sum);
 }
 
-function parseFirstPart(line: string) {
-    return line.split(": ")[1]!.split("; ").map(it => it.split(", ").map(l => l.split(" ") as [string, keyof ColoredBox]));
-}
 
 function isOverflowing(box: [string, keyof ColoredBox][]) {
   const LIMITS : ColoredBox = { red: 12, green: 13, blue: 14 };
@@ -31,3 +33,19 @@ function isOverflowing(box: [string, keyof ColoredBox][]) {
 firstPart();
 
 // Second Part
+console.log("Second Part");
+
+function secondPart() {
+  let sum = 0;
+  for(let id = 0; id < content.length; id++) {
+    const items = parse(content[id]!);
+    const minColors = { red: 0, green: 0, blue: 0 };
+    for(const sublist of items) {
+      sublist.forEach(item => minColors[item[1]] = Math.max(Number(item[0]), minColors[item[1]]));
+    }
+    const { red, green, blue } = minColors;
+    sum += red * green * blue;
+  }
+  console.log(sum);
+}
+secondPart();
