bdcea13fb4bb3971c8780516d7c079bc8f2c7ec7
minemobs <issahakim94@gmail.com>
2024-12-01 11:23:03 +0100

    Added 2nd part for day-01

--- day-01/index.ts
+++ day-01/index.ts
@@ -7,6 +7,19 @@ content.map(it => it.split("   ").map(it => Number(it)) as [number, number]).for
   arr[0].push(it[0]);
   arr[1].push(it[1]);
 });
-arr = arr.map(it => it.toSorted((a, b) => a - b));
-const sum = arr[0].map((it, i) => Math.abs(it - arr[1][i])).reduce((prev, curr) => prev + curr, 0);
-console.log("Sum", sum)
+
+function firstPart() {
+  arr = arr.map(it => it.toSorted((a, b) => a - b));
+  const sum = arr[0].map((it, i) => Math.abs(it - arr[1][i])).reduce((prev, curr) => prev + curr, 0);
+  console.log("Sum", sum)
+}
+
+function secondPart() {
+  const nbrOfOccurence: Map<number, number> = new Map();
+  let sum = 0;
+  for(const n of arr[0]) sum = n * (nbrOfOccurence.get(n) ?? arr[1].filter(it => it === n).length);
+  console.log("Sum:", sum);
+}
+
+if(process.env["PART"] === "1") firstPart();
+else secondPart();
